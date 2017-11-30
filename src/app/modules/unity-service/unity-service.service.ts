36f9edf15e8373de148ef1b14cd3b2ab039da91d
Christof Hullaert <christof.hullaert@cover3d.com>
2017-11-30 10:42:51 +0100

    add dictionary for methods

--- src/app/modules/unity-service/unity-service.service.ts
+++ src/app/modules/unity-service/unity-service.service.ts
@@ -5,9 +5,24 @@ import { UnityLoader } from 'unity-loader';
 
 declare let window: any;
 
+class UnityMethod {
+  constructor(public name: string) {
+  }
+}
+
+interface IDictionary {
+  [index: string]: UnityMethod;
+}
+
+interface IMessage {
+  type: string;
+  payload: any;
+}
+
 @Injectable()
 export class UnityService implements OnInit {
   private gameInstance: any;
+  private senders: IDictionary = {};
     
   constructor() { }
 
@@ -19,8 +34,14 @@ export class UnityService implements OnInit {
     this.gameInstance = UnityLoader.instantiate(componentId, "./assets/build.json");
   }
 
-  public sendMessage(messageHandler: string, message: any) {
-    this.gameInstance.SendMessage(messageHandler, message.type, JSON.stringify(message.payload));
+  public sendMessage(gameObject: string, message: IMessage) {
+    let method=this.senders[message.type] as UnityMethod;
+    if(method) 
+      this.gameInstance.SendMessage(gameObject, method.name, JSON.stringify(message.payload));    
+  }
+
+  public registerMessageSender(messageKey: string, unityMethod: UnityMethod) {
+    this.senders[messageKey]=unityMethod;
   }
 
 }
