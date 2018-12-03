e558f269ce6a031d261b8e98de847d0a2c77b18e
Christof Hullaert <christof.hullaert@cover3d.com>
2018-12-03 15:55:29 +0100

    chore(unity-viewer): need demo to visualize and verify conformity
    coding of a demo on default app component to start unity application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
+import { UnityLinkerModule } from './modules/unity-linker/unity-linker.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { UnityApplicationComponent } from './components/unity-application/unity-application.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    UnityApplicationComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, UnityLinkerModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
